daed976ee2b16bb19387ed32903b94d7fec010b3
Stavros <snikoletopoulos@hotmail.com>
2021-06-18 19:59:54 +0300

    Initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AssignmentTurnedInIcon from "@material-ui/icons/AssignmentTurnedIn";
+
+export default function NavBar() {
+	return (
+		<header>
+			<h1>
+				<AssignmentTurnedInIcon /> Keeper
+			</h1>
+		</header>
+	);
+}
---
+++ src/components/Note.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+
+export default function Note(props) {
+	const [deleteIcon, setDeleteIcon] = useState(false);
+
+	function handleHover(inButton) {
+		if (inButton) {
+			setDeleteIcon(true);
+		} else {
+			setDeleteIcon(false);
+		}
+	}
+
+	return (
+		<article className="note">
+			<h1>{props.title}</h1>
+			<p>{props.note}</p>
+			<button
+				onMouseEnter={() => handleHover(true)}
+				onMouseLeave={() => handleHover(false)}
+				type="submit"
+				onClick={(e) => {
+					e.preventDefault();
+					props.deleteFunc(props.arrayIndex);
+				}}>
+				{deleteIcon ? <DeleteForeverIcon /> : <DeleteIcon />}
+			</button>
+		</article>
+	);
+}
