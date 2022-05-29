7f700695dd4884d246ee30feb894ebae87c4ef79
Stavros <snikoletopoulos@hotmail.com>
2022-05-29 15:58:38 +0300

    Move App logic to Vite App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,41 @@
 import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
+// import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+// import Note from "components/Note";
+// import InputTask from "components/InputTask";
+
 function App() {
-	const [count, setCount] = useState(0);
+	const [tasks, setTasks] = useState([]);
+
+	const addTask = task => {
+		setTasks(prevValue => [...prevValue, task]);
+	};
+
+	const deleteTask = task => {
+		setTasks(prevValue => {
+			const newTaskList = [...prevValue];
+			newTaskList.splice(task, 1);
+			return newTaskList;
+		});
+	};
 
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>Hello Vite + React!</p>
-				<p>
-					<button type="button" onClick={() => setCount(count => count + 1)}>
-						count is: {count}
-					</button>
-				</p>
-				<p>
-					Edit <code>App.tsx</code> and save to test HMR updates.
-				</p>
-				<p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-					{" | "}
-					<a
-						className="App-link"
-						href="https://vitejs.dev/guide/features.html"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Vite Docs
-					</a>
-				</p>
-			</header>
-		</div>
+		<>
+			{/* <NavBar />
+			<InputTask handleClick={addTask} />
+			{tasks.map((note, index) => (
+				<Note
+					key={index}
+					arrayIndex={index}
+					deleteFunc={deleteTask}
+					title={note.title}
+					note={note.content}
+				/>
+			))} */}
+			<Footer />
+		</>
 	);
 }
 
